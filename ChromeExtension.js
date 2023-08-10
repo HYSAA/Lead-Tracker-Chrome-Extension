00577bf2fc0af84a562d0b853acc03361c094c43
HYSA <aj.anunciado@gmail.com>
2023-08-11 06:23:28 +0800

    updates

--- ChromeExtension.js
+++ ChromeExtension.js
@@ -1,6 +1,15 @@
-// Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
+const btn=document.getElementById("input-btn")
+const inputEl=document.getElementById("input-el").value
+let myLeads=[]
 
-let btn=document.getElementById("input-btn")
+
 btn.addEventListener("click",function(){
-console.log("button clicked")
+    // Push the value from the inputEl into the myLeads array 
+    // instead of the hard-coded "www.awesomeleads.com" value
+    // Google -> "get value from input field javascript"
+     
+    myLeads.push(inputEl)
+    console.log(myLeads)
+    console.log("Button clicked!")
 })
+
