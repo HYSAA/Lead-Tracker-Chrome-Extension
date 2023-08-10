dff3cf730a1a1b71be6030127a427db641e1b6b9
HYSA <aj.anunciado@gmail.com>
2023-08-11 04:41:58 +0800

    aaaaaaaaa

---
+++ ChromeExtension.js
@@ -0,0 +1,6 @@
+// Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
+
+let btn=document.getElementById("input-btn")
+btn.addEventListener("click",function(){
+console.log("button clicked")
+})
