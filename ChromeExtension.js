c15d65c87802240e5c942fbff10c5bffc0cd9f5f
HYSA <aj.anunciado@gmail.com>
2023-08-14 04:00:09 +0800

    will finish after school

--- ChromeExtension.js
+++ ChromeExtension.js
@@ -1,9 +1,8 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
-
+let myLeads = ["asdasd","sdaas"]
 const btn=document.getElementById("input-btn")
 const inputEl=document.getElementById("input-el")
 const ulEl=document.getElementById("ul-el")
-// 2. Grab the unordered list and store it in a const variable called 
+
 btn.addEventListener("click",function(){
      myLeads.push(inputEl.value)
     console.log(myLeads)
@@ -11,8 +10,7 @@ btn.addEventListener("click",function(){
  // Render the leads in the unordered list using ulEl.textContent
  for(let i=0;i < myLeads.length;i++)
  {
-    
-    ulEl.textContent+=myLeads[i]+" "
+ulEl.textContent+=myLeads[i]+" "
     console.log(myLeads[i])
  }
 
