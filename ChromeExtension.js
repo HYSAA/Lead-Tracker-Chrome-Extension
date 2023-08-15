d4faa5a93eb5ddc826a5d45503b78cc12c727e9f
HYSA <aj.anunciado@gmail.com>
2023-08-15 19:09:42 +0800

    aaaaaa

--- ChromeExtension.js
+++ ChromeExtension.js
@@ -1,17 +1,17 @@
-let myLeads = ["asdasd","sdaas"]
-const btn=document.getElementById("input-btn")
-const inputEl=document.getElementById("input-el")
-const ulEl=document.getElementById("ul-el")
+let myLeads = ["asdasd", "sdaas"]
+const btn = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
 
-btn.addEventListener("click",function(){
-     myLeads.push(inputEl.value)
+btn.addEventListener("click", function () {
+    myLeads.push(inputEl.value)
     console.log(myLeads)
 })
- // Render the leads in the unordered list using ulEl.textContent
- for(let i=0;i < myLeads.length;i++)
- {
-ulEl.textContent+=myLeads[i]+" "
+// Render the leads in the unordered list using ulEl.textContent
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.textContent += myLeads[i] + " "
     console.log(myLeads[i])
- }
+}
+function wait() {
 
- 
+}
