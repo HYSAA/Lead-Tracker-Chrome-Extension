aec1e9929519bfd1f0e2a7dec73469c0432ff19a
HYSA <aj.anunciado@gmail.com>
2023-08-24 04:25:31 +0800

    deploy

--- ChromeExtension.js
+++ ChromeExtension.js
@@ -3,6 +3,32 @@ const btn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 
+// 1. Save a key-value pair in localStorage
+// 2. Refresh the page. Get the value and log it to the console
+// 3. Clear localStorage
+
+// HINTS:
+// localStorage.setItem(key, value)
+// localStorage.getItem(key)
+// localStorage.clear()
+// PS: both key and value need to be strings
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 btn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     inputEl.value=""
@@ -13,7 +39,8 @@ const renderLeads = () => {
 let listItems=" "
 for (let i = 0; i < myLeads.length; i++) {
     // listItems += "<li><a  target='_blank' href='"+ myLeads[i] + "' >" + myLeads[i] + "</a></li>"
-    listItems += `
+  
+     listItems += `
     <li>
     <a target='_blank' href='  ${myLeads[i]}'> 
     ${myLeads[i]} </a> </li>
