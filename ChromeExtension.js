522e5dd63e5d313b215765d746eb2a84cb8fc1b6
HYSA <aj.anunciado@gmail.com>
2023-08-25 06:53:17 +0800

    asdasdsad

--- ChromeExtension.js
+++ ChromeExtension.js
@@ -1,34 +1,21 @@
-let myLeads = `["fn.com"]`
-// 1. Turn the myLeads string into an array
-// 2. Push a new value to the array
-// 3. Turn the array into a string again
-// 4. Console.log the string using typeof to verify that it's a string
-myLeads=JSON.parse(myLeads)//makes the string into an array
-myLeads.push("fb.com")
-myLeads=JSON.stringify(myLeads)//makes a array into a string
-console.log(typeof myLeads)
-
+let myLeads =[]
+// myLeads=JSON.parse(myLeads)//makes the string into an array
+// myLeads.push("fb.com")
+// myLeads=JSON.stringify(myLeads)//makes a array into a string
+// console.log(typeof myLeads)
 const btn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 
-
-
-localStorage.setItem("myLeads","youtube.com")
-
-let name= localStorage.getItem("myLeads")
-console.log(name)
-
-
-
-
-
-
-
 btn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     inputEl.value=""
+       // Save the myLeads array to localStorage 
+    // PS: remember JSON.stringify()
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
     renderLeads()
+
+    console.log(localStorage.getItem("myLeads"))
 })
 ////mo render sa screen
 const renderLeads = () => {
