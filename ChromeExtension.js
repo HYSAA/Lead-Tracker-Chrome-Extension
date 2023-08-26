e4f7e4e9c6702e94b140357972feece1a5fcdfd9
HYSA <aj.anunciado@gmail.com>
2023-08-26 21:26:27 +0800

    added delete button

--- ChromeExtension.js
+++ ChromeExtension.js
@@ -1,28 +1,22 @@
 let myLeads =[]
-// myLeads=JSON.parse(myLeads)//makes the string into an array
-// myLeads.push("fb.com")
-// myLeads=JSON.stringify(myLeads)//makes a array into a string
-// console.log(typeof myLeads)
 const btn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
-
-// Get the leads from the localStorage
-// Store it in a variable, leadsFromLocalStorage
-// Log out the variable
-let leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"))
-
-console.log(leadsFromLocalStorage)
+const delbtn = document.getElementById("delete-btn")
 btn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     inputEl.value=""
-       // Save the myLeads array to localStorage 
-    // PS: remember JSON.stringify()
     localStorage.setItem("myLeads",JSON.stringify(myLeads))
     renderLeads()
-
     console.log(localStorage.getItem("myLeads"))
 })
+delbtn.addEventListener("click",function(){
+  
+    if(ulEl)
+ulEl.textContent=""
+  localStorage.clear();
+
+});
 ////mo render sa screen
 const renderLeads = () => {
 let listItems=" "
@@ -35,3 +29,15 @@ for (let i = 0; i < myLeads.length; i++) {
 }
 ulEl.innerHTML=listItems
 }
+// const deleteEl = () =>{
+
+//  inputEl.innerHTML=""
+//  localStorage.clear();
+// }
+
+let leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"))
+if(leadsFromLocalStorage){
+  myLeads= leadsFromLocalStorage
+    renderLeads()
+}
+
