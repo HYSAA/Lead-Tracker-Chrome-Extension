595682791f219d41f4f20b2d63e3a7b31e77442a
HYSA <aj.anunciado@gmail.com>
2023-08-27 18:48:34 +0800

    changed the delete btn code

--- ChromeExtension.js
+++ ChromeExtension.js
@@ -1,53 +1,4 @@
-// let myLeads =[]
-// const btn = document.getElementById("input-btn")
-// const inputEl = document.getElementById("input-el")
-// const ulEl = document.getElementById("ul-el")
-// const delbtn = document.getElementById("delete-btn")
-// btn.addEventListener("click", function () {
-//   if(myLeads===null){
-//     myLeads=[]
-//     inputEl.value=""
-//     // localStorage.setItem("myLeads",JSON.stringify(myLeads))
-//     // renderLeads()
-//     // console.log(localStorage.getItem("myLeads"))
-//   }else{
-//     myLeads.push(inputEl.value)
-//     inputEl.value=""
-//     localStorage.setItem("myLeads",JSON.stringify(myLeads))
-//     renderLeads()
-//     console.log(localStorage.getItem("myLeads"))
-//   }
- 
-// })
-// delbtn.addEventListener("click",function(){
-//   deleteEl()
 
-// });
-// ////mo render sa screen
-// const renderLeads = () => {
-// let listItems=" "
-// for (let i = 0; i < myLeads.length; i++) {
-//      listItems += `
-//     <li>
-//     <a target='_blank' href='  ${myLeads[i]}'> 
-//     ${myLeads[i]} </a> </li>
-//     `
-// }
-// ulEl.innerHTML=listItems
-// }
-// const deleteEl = () =>{
-
-//   if(ulEl)
-// ulEl.textContent=""
-//   localStorage.clear();
-
-// }
-
-// let leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"))
-// if(leadsFromLocalStorage){
-//   myLeads= leadsFromLocalStorage
-//     renderLeads()
-// }
 const btn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
@@ -65,10 +16,15 @@ btn.addEventListener("click", function () {
   }
 });
 
-delbtn.addEventListener("click", function () {
-  myLeads = []; // Clear the array
-  ulEl.textContent = ""; // Clear the list content
-  localStorage.removeItem("myLeads"); // Remove data from localStorage
+delbtn.addEventListener("dblclick", function () {
+  //my code
+  // myLeads = []; // Clear the array
+  // ulEl.textContent = ""; // Clear the list content
+  // localStorage.removeItem("myLeads"); // Remove data from localStorage
+  localStorage.clear()
+  myLeads=[]
+  renderLeads()
+
 });
 
 const renderLeads = () => {
@@ -84,14 +40,14 @@ const renderLeads = () => {
   }
   ulEl.innerHTML = listItems;
 };
-
+//unused function
 const deleteEl = () => {
   myLeads = []; // Clear the array
   ulEl.textContent = ""; // Clear the list content
   localStorage.removeItem("myLeads"); // Remove data from localStorage
 };
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   renderLeads();
