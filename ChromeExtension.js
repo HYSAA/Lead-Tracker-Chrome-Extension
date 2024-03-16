50a1a70d968c53229a96ba456b46d875f0b066a2
Alberto Anunciado Jr <aj.anunciado@gmail.com>
2024-03-16 20:42:53 +0800

    will fix

--- ChromeExtension.js
+++ ChromeExtension.js
@@ -41,7 +41,7 @@ const renderLeads = (leads) => {
   for (let i = 0; i < leads.length; i++) {
     listItems += `
       <li>
-        <input type= "checkbox" checked>
+        <input type= "checkbox" id="lead-${i}" checked>
         <a target="_blank" href="${leads[i]}"> 
           ${leads[i]} 
         </a>
